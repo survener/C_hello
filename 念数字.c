eb84491c4fb7e42c7ef4fefd99d2c151e7d94fbd
sr <survener@163.com>
2022-01-06 10:25:31 +0800

    念数字

--- 念数字.c
+++ 念数字.c
@@ -53,16 +53,16 @@ int main()
         int d = num/t;
         switch (d)
         {
-        case 0:printf("ling");break;
-        case 1:printf("yi");break;
-        case 2:printf("er");break;
-        case 3:printf("san");break;
-        case 4:printf("si");break;
-        case 5:printf("wu");break;
-        case 6:printf("liu");break;
-        case 7:printf("qi");break;
-        case 8:printf("ba");break;
-        case 9:printf("jiu");break;
+        case 0:printf("ling");  break;
+        case 1:printf("yi");    break;
+        case 2:printf("er");    break;
+        case 3:printf("san");   break;
+        case 4:printf("si");    break;
+        case 5:printf("wu");    break;
+        case 6:printf("liu");   break;
+        case 7:printf("qi");    break;
+        case 8:printf("ba");    break;
+        case 9:printf("jiu");   break;
         }
         if ( t>9 ) printf(" ");
         num%=t;        
